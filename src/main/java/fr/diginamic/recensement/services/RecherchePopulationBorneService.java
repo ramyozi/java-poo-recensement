18b8b626f376f088302ba164899d5e5d57505895
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-18 15:03:09 +0200

    Try Catch added in RecherchePopulationBorneService

--- src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
+++ src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
@@ -18,26 +18,36 @@ public class RecherchePopulationBorneService extends MenuService {
 
 	@Override
 	public void traiter(Recensement rec, Scanner scanner) {
+		try {
+			System.out.println(
+					"Quel est le code du département recherché ? ");
+			String choix = scanner.nextLine();
 
-		System.out.println("Quel est le code du département recherché ? ");
-		String choix = scanner.nextLine();
+			System.out.println(
+					"Choississez une population minimum (en milliers d'habitants): ");
+			String saisieMin = scanner.nextLine();
 
-		System.out.println("Choississez une population minimum (en milliers d'habitants): ");
-		String saisieMin = scanner.nextLine();
-		
-		System.out.println("Choississez une population maximum (en milliers d'habitants): ");
-		String saisieMax = scanner.nextLine();
+			System.out.println(
+					"Choississez une population maximum (en milliers d'habitants): ");
+			String saisieMax = scanner.nextLine();
 
-		int min = Integer.parseInt(saisieMin) * 1000;
-		int max = Integer.parseInt(saisieMax) * 1000;
-		
-		List<Ville> villes = rec.getVilles();
-		for (Ville ville : villes) {
-			if (ville.getCodeDepartement().equalsIgnoreCase(choix)) {
-				if (ville.getPopulation() >= min && ville.getPopulation() <= max) {
-					System.out.println(ville);
+			int min = Integer.parseInt(saisieMin) * 1000;
+			int max = Integer.parseInt(saisieMax) * 1000;
+
+			List<Ville> villes = rec.getVilles();
+			for (Ville ville : villes) {
+				if (ville.getCodeDepartement().equalsIgnoreCase(choix)) {
+					if (ville.getPopulation() >= min
+							&& ville.getPopulation() <= max) {
+						System.out.println(ville);
+					}
 				}
 			}
+		} catch (NumberFormatException e) {
+			System.err.println(
+					"Erreur : Veuillez saisir un code de département valide (chiffres uniquement).");
+		} catch (IllegalArgumentException e) {
+			System.err.println("Erreur : " + e.getMessage());
 		}
 	}
 
